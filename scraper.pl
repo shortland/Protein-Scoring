da0390b482b6e94281724961be0c258b433125ec
ikleiman <ikleiman@cs.stonybrook.edu>
2018-04-30 22:18:26 -0400

    removed print statements meant for debugging

--- scraper.pl
+++ scraper.pl
@@ -12,7 +12,6 @@ BEGIN {
 	$work_book = Excel::Writer::XLSX->new("output.xlsx");
 	$xl_file = $work_book->add_worksheet();
 	$xl_row_count = 1;
-	# adds headers to the XL file
 	$xl_file->write(0, 0, "Protein Name");
 	$xl_file->write(0, 1, "Protein PBD");
 	$xl_file->write(0, 2, "Species");
@@ -23,7 +22,6 @@ BEGIN {
 	$xl_file->write(0, 7, "Global Symmetry");
 	$xl_file->write(0, 8, "Unique Protein Chains");
 	$xl_file->write(0, 9, "FASTA Chain(s)");
-	#$work_book->close;
 }
 
 sub write_data_to_excel {
@@ -41,23 +39,13 @@ sub get_depth_on_protein {
 	my $url = "https://www.rcsb.org/structure/";
 	my $uas = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
 	my $webpage_data = `curl -s -A "$uas" "$url$pbd" -L`;
-	print "Name: " . $name . "\n";
-	print "PBD: " . $pbd . "\n";
 	my ($classification) = ($webpage_data =~ /<li id="header_classification"><\w+>\w+\W&nbsp<\w \w+="\w+" href="[\w|\/|\.|\?|=|&|;| ]+">([\w| ]+)/);
-	print "Classification: " . $classification . "\n";
 	my ($species) = ($webpage_data =~ /<li id="header_organism"><\w+>\w+\(s\)\:&nbsp<\/\w+><a \w+="\w+" \w+="[\w|\/|\.|\?|=|&|;| ]+">([\w| ]+)/);
-	print "Species: " . $species . "\n";
 	my ($residue_count) = ($webpage_data =~ /id="contentResidueCount">Residue Count: ([\w]+)/);
-	print "Total Reside Count: " . $residue_count . "\n";
 	my ($atom_count) = ($webpage_data =~ /id="contentAtomSiteCount">Atom Count: ([\w]+)/);
-	print "Total Atom Count: " . $atom_count . "\n";
 	my ($structure_weight) = ($webpage_data =~ /id="contentStructureWeight">Total Structure Weight: ([\w]+)/);
-	print "Total Structure Weight: " . $structure_weight . "\n";
 	my ($global_symmetry) = ($webpage_data =~ /<strong>Global Symmetry<\/strong>: ([\w| |-]+)&/);
-	print "Global Symmetry: " . $global_symmetry . "\n";
 	my ($unique_protein_chains) = ($webpage_data =~ /id="contentProteinChainCount">Unique protein chains: ([\w]+)/);
-	print "Unique Protein Chains: " . $unique_protein_chains . "\n";
-	# there are multiple FASTFA chains. get all of em.
 	my $url;
 	my @fasta_datas;
 	for my $i (1 .. $unique_protein_chains) {
@@ -103,6 +91,5 @@ sub proteins_with_pbd {
 }
 
 BEGIN {
-	# site we are using to get proteins with a PBD number
 	proteins_with_pbd();
 }
